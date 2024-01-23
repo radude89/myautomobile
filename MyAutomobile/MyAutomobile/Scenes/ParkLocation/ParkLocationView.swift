695cc17540a29a3023820e8ce86ec24340d47aa0
radude89 <radu.ionut.dan@gmail.com>
2024-01-23 08:14:06 +0200

    Add basic camera controls

--- MyAutomobile/MyAutomobile/Scenes/ParkLocation/ParkLocationView.swift
+++ MyAutomobile/MyAutomobile/Scenes/ParkLocation/ParkLocationView.swift
@@ -5,31 +5,97 @@
 //  Created by Radu Dan on 15.10.2023.
 //
 
+import CoreLocation
 import SwiftUI
 import MapKit
 
 struct ParkLocationView: View {
+    
+    private let locationHandler = LocationManager()
+    @State private var cameraPosition = MapCameraPosition.userLocation(fallback: .automatic)
+    
     var body: some View {
         NavigationStack {
-            Map {
-                Marker("Parking", coordinate: .init(latitude: 1, longitude: 1))
-            }
-                .navigationTitle("Parking Spot")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button(action: addMarker) {
-                            Image(systemName: "plus")
-                        }
+            MapReader { proxy in
+                Map(position: $cameraPosition) {
+                    UserAnnotation()
+                }
+                .mapControls {
+                    MapUserLocationButton()
+                }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        print(coordinate)
                     }
                 }
+            }
+            .navigationTitle("Parking Spot")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: addMarker) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .onAppear {
+                locationHandler.requestLocation()
+            }
         }
     }
     
     private func addMarker() {
         
     }
+    
 }
 
-#Preview {
-    ParkLocationView()
+@Observable
+final class LocationManager: NSObject {
+    
+    private let locationHandler = CLLocationManager()
+
+    var userLocation: CLLocation?
+    
+    init(userLocation: CLLocation? = nil) {
+        self.userLocation = userLocation
+        super.init()
+        locationHandler.delegate = self
+        locationHandler.desiredAccuracy = kCLLocationAccuracyBest
+        locationHandler.startUpdatingLocation()
+    }
+    
+    func requestLocation() {
+        locationHandler.requestWhenInUseAuthorization()
+//        locationHandler.requestTemporaryFullAccuracyAuthorization(
+//            withPurposeKey: "parking"
+//        )
+    }
+    
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            print("notDetermined")
+        case .restricted:
+            print("restricted")
+        case .denied:
+            print("denied")
+        case .authorizedAlways:
+            print("authorizedAlways")
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+        @unknown default:
+            print("Unknown")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+
+        userLocation = location
+    }
 }
