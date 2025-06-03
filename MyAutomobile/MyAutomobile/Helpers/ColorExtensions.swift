184ea4ff64eb37a8aa6cb70e0d7e2382365b6661
radude89 <radu.ionut.dan@gmail.com>
2025-06-03 09:43:42 +0300

    Change vehicle image

--- MyAutomobile/MyAutomobile/Helpers/ColorExtensions.swift
+++ MyAutomobile/MyAutomobile/Helpers/ColorExtensions.swift
@@ -1,10 +1,3 @@
-//
-//  Color+Codable.swift
-//  MyAutomobile
-//
-//  Created by Radu Dan on 23.08.2023.
-//
-
 import SwiftUI
 #if os(iOS)
 import UIKit
@@ -22,13 +15,13 @@ extension Color {
     #else
     typealias SystemColor = UIColor
     #endif
-    
+
     var colorComponents: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
         var r: CGFloat = 0
         var g: CGFloat = 0
         var b: CGFloat = 0
         var a: CGFloat = 0
-        
+
         #if os(macOS)
         SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
         // Note that non RGB color will raise an exception, that I don't now how to catch because it is an Objc exception.
@@ -39,7 +32,7 @@ extension Color {
             return nil
         }
         #endif
-        
+
         return (r, g, b, a)
     }
 }
@@ -48,13 +41,13 @@ extension Color: Codable {
     enum CodingKeys: String, CodingKey {
         case red, green, blue
     }
-    
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let r = try container.decode(Double.self, forKey: .red)
         let g = try container.decode(Double.self, forKey: .green)
         let b = try container.decode(Double.self, forKey: .blue)
-        
+
         self.init(red: r, green: g, blue: b)
     }
 
@@ -62,9 +55,9 @@ extension Color: Codable {
         guard let colorComponents = self.colorComponents else {
             return
         }
-        
+
         var container = encoder.container(keyedBy: CodingKeys.self)
-        
+
         try container.encode(colorComponents.red, forKey: .red)
         try container.encode(colorComponents.green, forKey: .green)
         try container.encode(colorComponents.blue, forKey: .blue)
--- MyAutomobile/MyAutomobile/Scenes/Vehicle/CustomUI/VehicleImage.swift
+++ MyAutomobile/MyAutomobile/Scenes/Vehicle/CustomUI/VehicleImage.swift
@@ -9,26 +9,58 @@ import SwiftUI
 
 struct VehicleImage: View {
     let color: Color
-    
+
     var body: some View {
-        Image(systemName: "car.side.fill")
+        Image(systemName: "car")
             .resizable()
             .aspectRatio(contentMode: .fit)
             .symbolRenderingMode(.palette)
-            .foregroundStyle(color)
-            .padding(.all, 15)
-            .frame(width: 60, height: 60)
-            .clipShape(Circle())
+            .foregroundStyle(color, .secondary)
+            .padding(.all, 16)
+            .frame(width: 70, height: 70)
+            .background(
+                Circle()
+                    .fill(contrastingBackground(for: color))
+                    .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+            )
             .overlay(
                 Circle()
-                    .stroke(.foreground, lineWidth: 0.5)
-                    .shadow(radius: 2)
+                    .stroke(
+                        LinearGradient(
+                            colors: [
+                                color.opacity(0.3),
+                                color.opacity(0.1)
+                            ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        ),
+                        lineWidth: 1
+                    )
             )
+    }
+
+    private func contrastingBackground(for color: Color) -> Color {
+        let uiColor = UIColor(color)
+        var white: CGFloat = 0
+        uiColor.getWhite(&white, alpha: nil)
+
+        return white > 0.6 ? .black.opacity(0.5) : .white.opacity(0.5)
     }
 }
 
 struct VehicleImage_Previews: PreviewProvider {
     static var previews: some View {
-        VehicleImage(color: .cyan)
+        ScrollView {
+            VStack {
+                VehicleImage(color: .cyan)
+                VehicleImage(color: .white)
+                VehicleImage(color: .black)
+                VehicleImage(color: .pink)
+                VehicleImage(color: .yellow)
+                VehicleImage(color: .green)
+                VehicleImage(color: .mint)
+                VehicleImage(color: .blue)
+            }
+        }
     }
 }
